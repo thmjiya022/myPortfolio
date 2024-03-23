0aea422e0a4b345a560557be2dd4c3c345587241
Thobani <mjiyakhothobani@gmail.com>
2024-03-23 20:36:13 +0200

    static files

--- my_portfolio/settings.py
+++ my_portfolio/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -119,9 +120,16 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
-STATIC_URL = '/static/'
+STATIC_URL = '/static_files/'
+
+MEDIA_URL = '/images/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+# indicate the dir for static file(css, javascript, images)
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static_files')
+]
